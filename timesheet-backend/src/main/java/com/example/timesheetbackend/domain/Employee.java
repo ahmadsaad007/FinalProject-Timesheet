6381787f4a943b3a1c594319a0943318309f8be7
ahmadsaad007 <ahmadsd97@gmail.com>
2020-10-15 12:38:40 -0400

    Frontend missing files

--- timesheet-backend/src/main/java/com/example/timesheetbackend/domain/Employee.java
+++ timesheet-backend/src/main/java/com/example/timesheetbackend/domain/Employee.java
@@ -1,2 +1,94 @@
-package com.example.timesheetbackend.domain;public class Employee {
+package com.example.timesheetbackend.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "employee")
+public class Employee {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String phoneNumber;
+
+    private String emergencyContactNumber;
+    private String emergencyContactName;
+
+    public Employee(String id, String firstName, String lastName, String address, String email, String phoneNumber,
+                    String emergencyContactNumber, String emergencyContactName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.emergencyContactNumber = emergencyContactNumber;
+        this.emergencyContactName = emergencyContactName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmergencyContactNumber() {
+        return emergencyContactNumber;
+    }
+
+    public void setEmergencyContactNumber(String emergencyContactNumber) {
+        this.emergencyContactNumber = emergencyContactNumber;
+    }
+
+    public String getEmergencyContactName() {
+        return emergencyContactName;
+    }
+
+    public void setEmergencyContactName(String emergencyContactName) {
+        this.emergencyContactName = emergencyContactName;
+    }
 }
--- timesheet-backend/src/main/java/com/example/timesheetbackend/repo/EmployeeRepository.java
+++ timesheet-backend/src/main/java/com/example/timesheetbackend/repo/EmployeeRepository.java
@@ -1,2 +1,9 @@
-package com.example.timesheetbackend.repo;public class EmployeeRepository {
+package com.example.timesheetbackend.repo;
+
+import com.example.timesheetbackend.domain.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EmployeeRepository extends MongoRepository<Employee,String> {
+
+    Employee findFirstByFirstName(String firstName);
 }
