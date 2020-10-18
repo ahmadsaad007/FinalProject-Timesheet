f5e1461acf9c472d18ef5ddae5b593feaf3a3a65
ahmadsaad007 <ahmadsd97@gmail.com>
2020-10-18 11:08:18 -0400

    Sorry I added it directly to this branch

--- Backend/composite-microservice/src/main/java/com/example/compositemicroservice/controller/EmployeeClient.java
+++ Backend/composite-microservice/src/main/java/com/example/compositemicroservice/controller/EmployeeClient.java
@@ -1,2 +1,15 @@
-package com.example.compositemicroservice.controller;public class EmployeeClient {
+package com.example.compositemicroservice.controller;
+
+import com.example.compositemicroservice.domain.Employee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient("employeemicroservice")
+public interface EmployeeClient {
+
+    @GetMapping("/getAllEmployee")
+    List<Employee> getAllEmployees();
 }
