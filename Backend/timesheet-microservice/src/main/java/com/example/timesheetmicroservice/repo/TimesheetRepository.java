d437553142adeb552d7e8d7e1b906cecbcba6aa6
Zetian Wu <zetianwu@Zetians-MacBook-Pro.local>
2020-10-18 16:37:58 -0400

    timesheet update

---
+++ Backend/timesheet-microservice/src/main/java/com/example/timesheetmicroservice/repo/TimesheetRepository.java
@@ -0,0 +1,21 @@
+package com.example.timesheetmicroservice.repo;
+
+import com.example.timesheetmicroservice.domain.Timesheet;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TimesheetRepository extends MongoRepository<Timesheet,String> {
+
+    List<Timesheet> findAllByEid(String eid);
+    Timesheet findByEndDateAndEid(String endDate, String eid);
+
+    // find timesheet for a limited result by eid
+    // https://www.baeldung.com/spring-data-jpa-pagination-sorting
+//    List<Timesheet> findAllByEid(String eid, Pageable pageable);
+
+}
