6bff75025caa776333cc00093bc2302a87201b2b
ahmadsaad007 <ahmadsd97@gmail.com>
2020-10-19 15:24:27 -0400

    New changes

--- timesheet/src/components/TimeSheet.js
+++ timesheet/src/components/TimeSheet.js
@@ -40,12 +40,18 @@ componentDidMount() {
   console.log("Comp Mounted")
   axios.get(`http://localhost:8802/getSingleTimesheet?endDate=`+this.endDate+`&email=`+this.email)
      .then(res => {
-       //const {id,firstName, lastName, address, email, phoneNumber, emergencyContactNumber, emergencyContactName} = res.data;
-       console.log(res.data)
        this.setState({timesheets: res.data});
        this.setState({ isLoading: false });
        console.log(this.state.timesheets)
       })
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+
+    console.log(this.state.timesheets.timesheet.day1.endTime)
   }
 
   render() {
@@ -57,16 +63,16 @@ componentDidMount() {
         <div>
           <h2>TimeSheeet</h2>
           <div>
-            <label>Week Ending</label>
-            <input value={this.state.timesheets.endDate}></input>
+            <label>Week Ending: </label>
+            <input value={this.state.timesheets.endDate} disabled='true'></input>
           </div>
           <div>
-            <label>Total Billing Hours</label>
-            <input value={this.state.timesheets.billingHours}></input>
+            <label>Total Billing Hours:</label>
+            <input value={this.state.timesheets.billingHours} disabled='true'></input>
           </div>
           <div>
-            <label>Total Compensated Hours</label>
-            <input value={this.state.timesheets.totalHours}></input>
+            <label>Total Compensated Hours:</label>
+            <input value={this.state.timesheets.totalHours} disabled='true'></input>
           </div>
 
           <table>
@@ -84,71 +90,71 @@ componentDidMount() {
             </thead>
             <tbody>
               <tr>
-                <td> {this.state.timesheets.timesheet.day1.day}</td>
-                <td> {this.state.timesheets.timesheet.day1.date}</td>
-                <td> {this.state.timesheets.timesheet.day1.startTime}</td>
-                <td> {this.state.timesheets.timesheet.day1.endTime}</td>
-                <td> {this.state.timesheets.timesheet.day1.hours}</td>
-                <td><input type="radio" value={this.state.timesheets.timesheet.day1.floating}></input></td>
-                <td><input type="radio" value={this.state.timesheets.timesheet.day1.holiday}></input></td>
-                <td><input type="radio" value={this.state.timesheets.timesheet.day1.vaction}></input></td>  
+                <td>{this.state.timesheets.timesheet.day1.day}</td>
+                <td><input value={this.state.timesheets.timesheet.day1.date} disabled='true'></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day1.startTime} onChange={this.handleChange}></input></td>
+                <td><input value={this.state.timesheets.timesheet.day1.endTime} onChange={this.handleChange}></input></td>
+                <td><input value={this.state.timesheets.timesheet.day1.hours} onChange={this.handleChange}></input></td>
+                <td><input type="radio" value={this.state.timesheets.timesheet.day1.floating} onChange={this.handleChange}></input></td>
+                <td><input type="radio" value={this.state.timesheets.timesheet.day1.holiday} onChange={this.handleChange}></input></td>
+                <td><input type="radio" value={this.state.timesheets.timesheet.day1.vaction} onChange={this.handleChange}></input></td>  
               </tr>
               <tr>
                 <td> {this.state.timesheets.timesheet.day2.day}</td>
-                <td> {this.state.timesheets.timesheet.day2.date}</td>
-                <td> {this.state.timesheets.timesheet.day2.startTime}</td>
-                <td> {this.state.timesheets.timesheet.day2.endTime}</td>
-                <td> {this.state.timesheets.timesheet.day2.hours}</td>
-                <td><input type="radio" value={this.state.timesheets.timesheet.day2.floating}></input></td>
-                <td><input type="radio" value={this.state.timesheets.timesheet.day2.holiday}></input></td>
-                <td><input type="radio" value={this.state.timesheets.timesheet.day2.vaction}></input></td>  
+                <td><input value= {this.state.timesheets.timesheet.day2.date} disabled='true'></input></td>
+                <td><input value={this.state.timesheets.timesheet.day2.startTime} onChange={this.handleChange}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day2.endTime} onChange={this.handleChange}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day2.hours} onChange={this.handleChange}></input></td>
+                <td><input type="radio" value={this.state.timesheets.timesheet.day2.floating} onChange={this.handleChange}></input></td>
+                <td><input type="radio" value={this.state.timesheets.timesheet.day2.holiday} onChange={this.handleChange}></input></td>
+                <td><input type="radio" value={this.state.timesheets.timesheet.day2.vaction} onChange={this.handleChange}></input></td>  
               </tr>
               <tr>
                 <td> {this.state.timesheets.timesheet.day3.day}</td>
-                <td> {this.state.timesheets.timesheet.day3.date}</td>
-                <td> {this.state.timesheets.timesheet.day3.startTime}</td>
-                <td> {this.state.timesheets.timesheet.day3.endTime}</td>
-                <td> {this.state.timesheets.timesheet.day3.hours}</td>
+                <td><input value={this.state.timesheets.timesheet.day3.date} disabled='true'></input></td>
+                <td><input value={this.state.timesheets.timesheet.day3.startTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day3.endTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day3.hours}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day3.floating}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day3.holiday}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day3.vaction}></input></td>  
               </tr>
               <tr>
                 <td> {this.state.timesheets.timesheet.day4.day}</td>
-                <td> {this.state.timesheets.timesheet.day4.date}</td>
-                <td> {this.state.timesheets.timesheet.day4.startTime}</td>
-                <td> {this.state.timesheets.timesheet.day4.endTime}</td>
-                <td> {this.state.timesheets.timesheet.day4.hours}</td>
+                <td><input value= {this.state.timesheets.timesheet.day4.date} disabled='true'></input></td>
+                <td><input value={this.state.timesheets.timesheet.day4.startTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day4.endTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day4.hours}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day4.floating}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day4.holiday}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day4.vaction}></input></td>  
               </tr>
               <tr>
                 <td> {this.state.timesheets.timesheet.day5.day}</td>
-                <td> {this.state.timesheets.timesheet.day5.date}</td>
-                <td> {this.state.timesheets.timesheet.day5.startTime}</td>
-                <td> {this.state.timesheets.timesheet.day5.endTime}</td>
-                <td> {this.state.timesheets.timesheet.day5.hours}</td>
+                <td><input value= {this.state.timesheets.timesheet.day5.date} disabled='true'></input></td>
+                <td><input value={this.state.timesheets.timesheet.day5.startTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day5.endTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day5.hours}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day5.floating}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day5.holiday}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day5.vaction}></input></td>  
               </tr>
               <tr>
                 <td> {this.state.timesheets.timesheet.day6.day}</td>
-                <td> {this.state.timesheets.timesheet.day6.date}</td>
-                <td> {this.state.timesheets.timesheet.day6.startTime}</td>
-                <td> {this.state.timesheets.timesheet.day6.endTime}</td>
-                <td> {this.state.timesheets.timesheet.day6.hours}</td>
+                <td><input value= {this.state.timesheets.timesheet.day6.date} disabled='true'></input></td>
+                <td><input value={this.state.timesheets.timesheet.day6.startTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day6.endTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day6.hours}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day6.floating}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day6.holiday}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day6.vaction}></input></td>  
               </tr>
               <tr>
                 <td> {this.state.timesheets.timesheet.day7.day}</td>
-                <td> {this.state.timesheets.timesheet.day7.date}</td>
-                <td> {this.state.timesheets.timesheet.day7.startTime}</td>
-                <td> {this.state.timesheets.timesheet.day7.endTime}</td>
-                <td> {this.state.timesheets.timesheet.day7.hours}</td>
+                <td><input value={this.state.timesheets.timesheet.day7.date} disabled='true'></input></td>
+                <td><input value={this.state.timesheets.timesheet.day7.startTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day7.endTime}></input></td>
+                <td><input value= {this.state.timesheets.timesheet.day7.hours}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day7.floating}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day7.holiday}></input></td>
                 <td><input type="radio" value={this.state.timesheets.timesheet.day7.vaction}></input></td>  
@@ -158,7 +164,7 @@ componentDidMount() {
           </table>
 
           <div>
-            <label>Upload Timesheet</label>
+            <label>Upload Timesheet:  </label>
             <input type="file"></input>
           </div>
           <button type='submit'>Save</button>
